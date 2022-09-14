f902a716e21cddfadc7275f96e2f640e619b4f11
Nischey Gupta <ngupta16@my.bcit.ca>
2022-09-14 12:35:30 -0700

    commit for attendance

--- src/main/java/comp2522/greeting.java
+++ src/main/java/comp2522/greeting.java
@@ -2,6 +2,6 @@ package comp2522;
 
 public class greeting {
     public static void main(String[] args) {
-        System.out.println("Hello everyone");
+        System.out.println("I am present");
     }
 }
